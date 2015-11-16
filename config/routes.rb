af9567b2ef748fae3f3e1997322bff70496e7c91
Javix <s.cambour@gmail.com>
2015-11-16 22:21:54 +0100

    Modify routes: move back office resources to admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :subjects, shallow: true do
-    resources :themes, shallow: true, except: :index do
-      resources :questions, shallow: true, except: :index do
-        resources :answers, except: :index
+
+  scope '/admin' do
+    devise_for :users
+    resources :subjects, shallow: true do
+      resources :themes, shallow: true, except: :index do
+        resources :questions, shallow: true, except: :index do
+          resources :answers, except: :index
+        end
       end
     end
+
+    resources :themes,    only: :index
+    resources :questions, only: :index
+    resources :answers,   only: :index
   end
-
-  resources :themes,    only: :index
-  resources :questions, only: :index
-  resources :answers,   only: :index
-
-  devise_for :users
 end
