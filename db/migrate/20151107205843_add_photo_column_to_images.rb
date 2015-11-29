2f2ef8c956fe25e347f23d0d67471d72b6fb92f0
Javix <s.cambour@gmail.com>
2015-11-29 19:40:25 +0100

    Correct indentation

--- db/migrate/20151107205843_add_photo_column_to_images.rb
+++ db/migrate/20151107205843_add_photo_column_to_images.rb
@@ -1,9 +1,9 @@
 class AddPhotoColumnToImages < ActiveRecord::Migration
   def up
-      add_attachment :images, :photo
-    end
+    add_attachment :images, :photo
+  end
 
-    def down
-      remove_attachment :images, :photo
-    end
+  def down
+    remove_attachment :images, :photo
+  end
 end
