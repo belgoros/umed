24047680421ac2ee45185a7a33103d3b7c50b03b
Javix <s.cambour@gmail.com>
2016-01-19 13:47:59 +0100

    Add validation for question_id

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,5 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   scope :correct, -> (ids = []) { where(id: ids, correct: true).pluck(:id) }
   validates :text, presence: true
+  validates :question_id, presence: true
 end
