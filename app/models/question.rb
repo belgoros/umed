02e960ebaee484a147c96e3024fd7fbf6f9114b1
Javix <s.cambour@gmail.com>
2016-01-22 12:10:12 +0100

    Return random question IDs for the quiz
    - use ‘postgresql’ specific function RANDOM()

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
   has_many :images, dependent: :destroy
   belongs_to :theme
-
-  scope :ids_for_quiz, ->(theme_id = null, limit = 5) { where(theme_id: theme_id).limit(limit).pluck(:id).map(&:to_s) }
+  #postgresql specific query using RANDOM() function
+  scope :ids_for_quiz, ->(theme_id = null, limit = 5) { where(theme_id: theme_id).limit(limit).order("RANDOM()").pluck(:id).map(&:to_s) }
   validates :text, presence: true
 end
