4417b2226e63b18c595b883365851dafeed005ef
Javix <s.cambour@gmail.com>
2016-01-25 16:13:36 +0100

    Save question_ids and answer_ids as Integer
    - modify scope method #ids_for_quiz and remove #to_s call
    - indent private methods

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,7 @@ class Question < ActiveRecord::Base
   has_many :images, dependent: :destroy
   belongs_to :theme
   #postgresql specific query using RANDOM() function
-  scope :ids_for_quiz, ->(theme_id = null, limit = 5) { where(theme_id: theme_id).limit(limit).order("RANDOM()").pluck(:id).map(&:to_s) }
+  scope :ids_for_quiz, ->(theme_id = null, limit = 5) { where(theme_id: theme_id).limit(limit).order("RANDOM()").pluck(:id) }
+
   validates :text, presence: true
 end
