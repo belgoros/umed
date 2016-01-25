1d83e567561ca6433722ded4f2c972ecf67eda19
Javix <s.cambour@gmail.com>
2016-01-25 17:40:26 +0100

    Use DEFAULT_QUESTIONS_NUMBER
    - use the constant value to query questions ids for quiz

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,10 @@
 class Question < ActiveRecord::Base
+  DEFAULT_QUESTIONS_NUMBER = 5
   has_many :answers, dependent: :destroy
   has_many :images, dependent: :destroy
   belongs_to :theme
   #postgresql specific query using RANDOM() function
-  scope :ids_for_quiz, ->(theme_id, limit = 5) { where(theme_id: theme_id).limit(limit).order("RANDOM()").pluck(:id) }
+  scope :ids_for_quiz, ->(theme_id, limit = DEFAULT_QUESTIONS_NUMBER) { where(theme_id: theme_id).limit(limit).order("RANDOM()").pluck(:id) }
 
   validates :text, presence: true
   validates :theme_id, presence: true
