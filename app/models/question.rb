b293989625273d346700822787af43da1ca8e2f3
Javix <s.cambour@gmail.com>
2016-01-25 17:29:48 +0100

    Add theme_id validation, remove default nil value for theme_id in #ids_for_quiz

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,8 @@ class Question < ActiveRecord::Base
   has_many :images, dependent: :destroy
   belongs_to :theme
   #postgresql specific query using RANDOM() function
-  scope :ids_for_quiz, ->(theme_id = null, limit = 5) { where(theme_id: theme_id).limit(limit).order("RANDOM()").pluck(:id) }
+  scope :ids_for_quiz, ->(theme_id, limit = 5) { where(theme_id: theme_id).limit(limit).order("RANDOM()").pluck(:id) }
 
   validates :text, presence: true
+  validates :theme_id, presence: true
 end
