a2b1b1b7f902a38c9df1d239883269366c9128d9
Javix <s.cambour@gmail.com>
2016-02-01 16:53:57 +0100

    Add heroku app host to send mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'umed.herokuapp.com' }
   # SMTP settings for mailgun
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
