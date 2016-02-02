4eb458868b078e156b82cfde6c2c8cc368b865c8
Javix <s.cambour@gmail.com>
2016-02-02 10:51:30 +0100

    Add empty line

--- spec/models/level_spec.rb
+++ spec/models/level_spec.rb
@@ -4,6 +4,7 @@ describe Level, type: :model do
 
   context 'validations' do
     subject { build(:level)}
+
     it { should validate_uniqueness_of(:name).case_insensitive }
     it { should validate_presence_of(:name)}
   end
