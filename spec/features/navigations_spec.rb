d2161f55630213cd3a3ef8c2475adf55cacaf94a
Javix <s.cambour@gmail.com>
2016-02-04 13:12:40 +0100

    Add navigation links feature

---
+++ spec/features/navigations_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature "Navigations", type: :feature do
+  scenario "Admin user should see all the links in the navbar" do
+    user = create(:admin)
+    signin_user(user.email, user.password)
+    expect(page).to have_link(I18n.t('links.take_quiz'))
+    expect(page).to have_link(I18n.t('links.levels'))
+    expect(page).to have_link(I18n.t('links.subjects'))
+    expect(page).to have_link(I18n.t('links.themes'))
+    expect(page).to have_link(I18n.t('links.questions'))
+    expect(page).to have_link(I18n.t('links.answers'))
+    expect(page).to have_link(I18n.t('links.dashboard'))
+  end
+
+  scenario "Not admin user should not admin links" do
+    user = create(:user)
+    signin_user(user.email, user.password)
+    expect(page).to have_link(I18n.t('links.take_quiz'))
+    expect(page).not_to have_link(I18n.t('links.levels'))
+    expect(page).not_to have_link(I18n.t('links.subjects'))
+    expect(page).not_to have_link(I18n.t('links.themes'))
+    expect(page).not_to have_link(I18n.t('links.questions'))
+    expect(page).not_to have_link(I18n.t('links.answers'))
+    expect(page).to have_link(I18n.t('links.dashboard'))
+  end
+end
