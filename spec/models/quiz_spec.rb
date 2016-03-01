729556b23d9703a55e77c968b43a71085111f6e6
Javix <s.cambour@gmail.com>
2016-03-01 11:31:29 +0100

    Add the spec example for #answers

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -15,6 +15,20 @@ describe Quiz, type: :model do
     it { should validate_presence_of(:theme_id)}
   end
 
+  context "#answers" do
+    before(:each) { @quiz = create(:quiz)}
+
+    it "should return quiz answers" do
+      setup_quiz_answers(@quiz)
+      expect(@quiz.answers).not_to be_empty
+    end
+
+    it "should return no answers" do
+      @quiz.update_attributes(level_id: nil, subject_id: nil, theme_id: nil)
+      expect(@quiz.answers).to be_empty
+    end
+  end
+
   context "return completed quizzes" do
     let(:quiz) { create(:quiz)}
 
@@ -35,5 +49,14 @@ describe Quiz, type: :model do
       quiz.theme = nil
       expect(quiz.complete?).to be false
     end
+  end
+
+  def setup_quiz_answers(quiz)
+    questions = create_list(:question, 3)
+    answers = []
+    questions.each do |question|
+      answers << create(:answer, question: question)
+    end
+    @quiz.update_attributes(question_ids: questions.map(&:id), answer_ids: answers.map(&:id))
   end
 end
