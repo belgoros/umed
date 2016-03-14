474935526595c5573acd8bf662c2fdb0222b0ed9
Javix <s.cambour@gmail.com>
2016-03-14 10:36:52 +0100

    Redirect to quiz details in the end of the quiz
    - add find_quiz and add before_action filter

--- app/controllers/quiz/questions_controller.rb
+++ app/controllers/quiz/questions_controller.rb
@@ -1,23 +1,21 @@
 class Quiz::QuestionsController < ApplicationController
   include Wicked::Wizard
 
+  before_action :find_quiz, only: [:show, :update]
   before_action :set_steps
   before_action :setup_wizard
 
   def show
     if step == Wicked::FINISH_STEP
-      redirect_to finish_wizard_path, notice: t(:quiz_finished_success, model: Quiz.model_name.human)
+      redirect_to details_quiz_path(@quiz), notice: t(:quiz_finished_success, model: Quiz.model_name.human)
     else
-      @quiz = Quiz.find(params[:quiz_id])
       @question = Question.find(step)
-
       render_wizard nil, { template: 'quiz/questions/question'}
     end
 
   end
 
   def update
-    @quiz = Quiz.find(params[:quiz_id])
     @quiz.answer_ids.push(params[:quiz][:answer_ids].to_i) if params[:quiz] && !params[:quiz][:answer_ids].blank?
     #No need to save the quiz, render_wizzard saves it when passing in an object
     render_wizard @quiz
@@ -29,9 +27,12 @@ class Quiz::QuestionsController < ApplicationController
 
   private
 
+    def find_quiz
+      @quiz = Quiz.find(params[:quiz_id])
+    end
+
     def set_steps
-      quiz = Quiz.find(params[:quiz_id])
-      question_ids = quiz.question_ids
+      question_ids = @quiz.question_ids
       self.steps = question_ids.map(&:to_s)
     end
 
