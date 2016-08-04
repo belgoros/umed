c11ee95fb7b53f6bec5483f18828479c0061763b
Javix <s.cambour@gmail.com>
2016-08-04 23:09:15 +0200

    Add #delete_old_quizzes to be used by rake task (WIP)

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,16 @@ class User < ActiveRecord::Base
 
    after_create :send_welcome_email
 
+  def self.delete_old_quizzes
+    old_quizzes = []
+    users = includes(:quizzes)
+    users.each do |user|
+      old_quizzes =+ user.quizzes.offset(25) unless user.quizzes.empty?
+    end
+
+    old_quizzes.delete_all
+  end
+
 
    private
    def send_welcome_email
