aa7d2b3434c8dc86920a20ce7f67110b99b1d7dc
Javix <s.cambour@gmail.com>
2016-11-13 16:00:00 +0100

    Add #enroll_for_days
    * add #enroll_for_days to Subscription
    * add new spec examples to subscription_spec

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -24,6 +24,7 @@ describe Subscription, type: :model do
       subscription.save(validate: false)
       subscription.enroll_for_plan(premium_plan)
       expect(subscription.start_date).to eq Date.today
+      expect(subscription.end_date).to eq subscription.start_date.advance(months: 1)
     end
 
     it "should enroll for premium plan with existing activated subscription" do
@@ -34,7 +35,35 @@ describe Subscription, type: :model do
       subscription.enroll_for_plan(premium_plan)
       expect(subscription.start_date).to eq @end_date.advance(days: 1)
       expect(subscription.end_date).to eq subscription.start_date.advance(months: 1)
+    end
+  end
 
+  context "offered subscription" do
+    let(:premium_plan) { create(:premium_plan)}
+
+    it "should enroll a user for a premium plan with a specified duration and existing subscription" do
+      offered_days = 10
+      setup_dates
+      subscription = build(:subscription, plan: premium_plan, start_date: @start_date, end_date: @end_date)
+      subscription.save(validate: false)
+
+      subscription.enroll_for_days(premium_plan, offered_days)
+      expect(subscription.start_date).to eq @end_date.advance(days: 1)
+      expect(subscription.end_date).to eq subscription.start_date.advance(days: offered_days)
+      expect(subscription.user.premium).to be_truthy
+      expect(subscription.activated).to be_truthy
+    end
+
+    it "should enroll a user for a premium plan with a specified duration and no active subscription" do
+      offered_days = 10
+      subscription = build(:subscription, plan: premium_plan)
+      subscription.save(validate: false)
+
+      subscription.enroll_for_days(premium_plan, offered_days)
+      expect(subscription.start_date).to eq Date.today
+      expect(subscription.end_date).to eq subscription.start_date.advance(days: offered_days)
+      expect(subscription.user.premium).to be_truthy
+      expect(subscription.activated).to be_truthy
     end
   end
 end
