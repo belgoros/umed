f6d1df653666f2f2610f6fdd1baa3b57275139cd
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-09-17 09:45:51 +0200

    Fix warning about using RANDOM function
    DEPRECATION WARNING: Dangerous query method (method whose arguments are used as raw SQL) called with non-attribute argument(s): "random()". Non-attribute arguments will be disallowed in Rails 6.0. This method should not be called with user-provided values, such as request parameters or model attributes. Known-safe values can be passed by wrapping them in Arel.sql(). (called from …).
    See https://github.com/rails/rails/issues/32995 for more discussions details.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,12 @@
 class Question < ApplicationRecord
   DEFAULT_QUESTIONS_NUMBER = 5
+
+  #postgresql specific query using RANDOM() function
+  RANDOM_ORDER = Arel.sql('random()')
   # Ordering question answers randomly to avoid guessing when passing a quiz and
   # make it easier to create answers and not forget to have the correct one
   # (starting from from the correct one, for example, then adding the wrong ones)
-  has_many :answers, -> { order "RANDOM()"}, dependent: :destroy
+  has_many :answers, -> { order RANDOM_ORDER }, dependent: :destroy
   has_attached_file :photo,
                     styles: { medium: "100x100>", thumb: "150x150>" }, default_url: "/images/:style/missing.png"
   validates_attachment :photo,
@@ -20,8 +23,7 @@ class Question < ApplicationRecord
 
   def self.ids_for_quiz(user, theme_id, limit = DEFAULT_QUESTIONS_NUMBER)
     if user.premium?
-      #postgresql specific query using RANDOM() function
-      where(theme_id: theme_id).limit(limit.nil? ? DEFAULT_QUESTIONS_NUMBER : limit).order("RANDOM()").pluck(:id)
+      where(theme_id: theme_id).limit(limit.nil? ? DEFAULT_QUESTIONS_NUMBER : limit).order(RANDOM_ORDER).pluck(:id)
     else
       where(theme_id: theme_id).pluck(:id).first(DEFAULT_QUESTIONS_NUMBER)
     end
